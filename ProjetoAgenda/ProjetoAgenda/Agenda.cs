5ad5cbd4821a3cca7d5d41cf9c45b402a8956765
Alexandre Gomes Vieira <84604919+Alexandregv1080p@users.noreply.github.com>
2023-03-28 20:27:33 -0300

    Add files via upload

---
+++ ProjetoAgenda/ProjetoAgenda/Agenda.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAgenda
+{
+    internal class Agenda
+    {
+        private string cidade;
+        private string[] fone;
+        private string nome;
+        private int codigo;
+
+        public Agenda(string cidade, string nome, int codigo)
+        {
+            this.Cidade = cidade;
+            string[] fone = new string[3];
+            this.Nome = nome;
+            this.Codigo = codigo;
+        }
+
+        public string Cidade { get => cidade; set => cidade = value; }
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nome { get => nome; set => nome = value ;}
+        public string[] Fone { get => fone; set => fone = value; }
+
+        string msg = "";
+        public string mostra()
+        {
+
+            msg+= "Cidade : " + Cidade + ", Codigo : " + Codigo + ", Nome : " + Nome + ", Telefones : ";
+            foreach (var f in fone)
+            {
+                msg += f + "\n";
+            }
+            return msg;
+        }
+    }
+}
---
+++ ProjetoAgenda/ProjetoAgenda/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAgenda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Agenda> agenda = new List<Agenda>();
+            Agenda A;
+            char ok = 's';
+            do
+            {
+                
+                Console.Write("Digite seu nome : ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite sua cidade : ");
+                string cidade = Console.ReadLine();
+                Console.Write("Digite seu codigo : ");
+                int codigo = int.Parse(Console.ReadLine());
+                string[] fones = new string[3];
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Entre com o " + (i+1) + "o numero ");
+                    fones[i] = Console.ReadLine();
+                }
+                A = new Agenda(cidade,nome,codigo);
+                A.Fone = fones;
+                agenda.Add(A);
+                Console.WriteLine("Continuar? (s/n)");
+                ok = char.Parse(Console.ReadLine());
+            } while (ok == 's');
+            foreach(Agenda ag in agenda)
+            {
+                Console.WriteLine(ag.mostra());
+            }
+            Console.ReadKey();
+        }
+    }
+}
