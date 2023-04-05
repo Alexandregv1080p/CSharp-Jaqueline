d31a4dea896052ac2e48ecea353b7d5c4d003818
Alexandre Gomes Vieira <84604919+Alexandregv1080p@users.noreply.github.com>
2023-04-05 19:06:23 -0300

    Update Program.cs

--- Aula3/Aula3/Program.cs
+++ Aula3/Aula3/Program.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Aula3
 {
     internal class Program
@@ -11,7 +10,7 @@ namespace Aula3
         static void Main(string[] args)
         {
             List<Atleta> listaAtletas = new List<Atleta>();
-            List<string> modalidades = new List<string>();
+            HashSet<string> modalidades2 = new HashSet<string>();
             Atleta A;
             string nome;
             int op;
@@ -19,68 +18,148 @@ namespace Aula3
             {
                 Console.WriteLine("---Registro de atletas---");
                 Console.WriteLine("1 - Criar atleta");
-                Console.WriteLine("2 - Pesquisar o nome dos atletas por modalidade");
-                Console.WriteLine("3 - Mostrar o nome dos atletas menores de 21 anos");
-                Console.WriteLine("4 - Mostrar os atletas de uma determinada cidade");
-                Console.WriteLine("5 - Mostrar o numero de atletas de cada modalidade");
+                Console.WriteLine("2 - Pesquisar o nome dos atletas por
+                modalidade");
+                Console.WriteLine("3 - Mostrar o nome dos atletas menores de 21
+                anos");
+                Console.WriteLine("4 - Mostrar os atletas de uma determinada
+                cidade");
+                Console.WriteLine("5 - Mostrar o numero de atletas de cada
+                modalidade");
                 Console.WriteLine("6 - Sair");
                 op = int.Parse(Console.ReadLine());
                 if (op == 1)
                 {
+                    Console.Clear();
+
                     Console.Write("Digite o nome do atleta: ");
+
                     nome = Console.ReadLine();
                     A = new Atleta(nome);
                     Console.Write("Digite a cidade do atleta: ");
+
                     A.Cidade = Console.ReadLine();
                     Console.Write("Digite a idade do atleta: ");
                     A.Idade = int.Parse(Console.ReadLine());
-                    Console.Write("Entre com o numero de modalidades que deseja gravar: ");
+                    Console.Write("Entre com o numero de modalidades que deseja
+                    
+
+                    gravar: ");
                     int n = int.Parse(Console.ReadLine());
-               
-                    for(int i = 0; i < n; i++)
+                    List<string> modalidades = new List<string>();
+                    for (int i = 0; i < n; i++)
                     {
                         Console.Write($"Digite a {i + 1}a modalidade: ");
                         string mod = Console.ReadLine();
                         modalidades.Add(mod);
+                        modalidades2.Add(mod);
                     }
+
                     A.ListaModalidade = modalidades;
                     listaAtletas.Add(A);
+                    modalidades2.Distinct().ToList();
+                    Console.Clear();
                     Console.WriteLine("Atleta adicionado com sucesso!");
+
+                    Console.WriteLine("Aperte enter para continuar....");
+
+                    Console.ReadKey();
+                    Console.Clear();
                 }
-                if(op == 2)
+                if (op == 2)
                 {
-                    Console.Write("Digite o nome da modalidade em que deseja procurar atletas: ");
+                    Console.Clear();
+
+                    Console.Write("Digite o nome da modalidade em que deseja
+                    
+
+                    procurar atletas: ");
                     nome = Console.ReadLine();
-                   
-                }
-                if(op == 3)
-                {
-                    Console.WriteLine("Alunos abaixo de 21 anos : ");
-                    foreach(Atleta a in listaAtletas)
+                    foreach (Atleta a in listaAtletas)
                     {
-                        if(a.Idade < 21)
+                        Console.WriteLine(a.mostrarModalidade(nome));
+                        if (a.mostrarModalidade(nome) == true)
+
                         {
-                            a.mostrarDados();
+                            Console.WriteLine(a.mostrarDados());
                         }
                     }
-                }
-                if(op == 4)
-                {
-                    Console.Write("Digite o nome da cidade: ");
-                    nome = Console.ReadLine();
-                    foreach(Atleta i in listaAtletas)
-                    {
-                        if(i.Cidade == nome)
-                        {
-                            i.mostrarDados();
-                        }
-                    }
-                }
-                if(op == 5)
-                {
+                    Console.WriteLine("Aperte enter para continuar....");
+
+                    Console.ReadKey();
+                    Console.Clear();
 
                 }
+                if (op == 3)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Alunos abaixo de 21 anos : ");
+                    foreach (Atleta a in listaAtletas)
+
+                    {
+                        if (a.Idade < 21)
+
+                        {
+
+                            Console.WriteLine(a.mostrarDados());
+                        }
+                    }
+
+                    Console.WriteLine("Aperte enter para continuar....");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+                if (op == 4)
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Alunos cadastrados na cidade");
+                    Console.Write("Digite o nome da cidade: ");
+                    nome = Console.ReadLine();
+                    foreach (Atleta i in listaAtletas)
+
+                    {
+                        if (i.Cidade == nome)
+
+                        {
+
+                            Console.WriteLine(i.mostrarDados());
+                        }
+                    }
+
+                    Console.WriteLine("Aperte enter para continuar....");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+                if (op == 5)//nao consegui fazer a 5
+
+                {
+                    Console.Clear();
+
+                    Console.WriteLine("Numero de atletas registrados em cada
+                    
+
+                    modalidade: ");
+                    int v = 1;
+
+                    foreach (Atleta a in listaAtletas)
+
+                    {
+                        foreach (string i in modalidades2)
+                        {
+                            if (a.mostrarModalidade(i) == true)
+                            {
+                                Console.WriteLine(i + ":" + v);
+                                v++;
+                            }
+                        }
+                    }
+                }
             } while (op != 6);
+            Console.Clear();
             Console.WriteLine("Programa finalizado!");
             Console.ReadKey();
         }
